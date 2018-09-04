ee70751c5d6ae17e395d1cf0a67ae0f229b9b8d1
AbdulAzeez <aatanda.dammy@gmail.com>
2018-09-04 14:27:47 +0100

    Set up router for version 1 of the api

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
 module.exports = {
-    "extends": "airbnb-base"
+    "extends": "airbnb-base",
+    "env" :{
+        "node": true,
+        "es6": true,
+        "mocha": true
+    },
+    "rules":{
+        "linebreak-style": 0
+    }
 };
--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-module.exports = () => 'hello';
+const express = require('express');
+
+const apiVersion1 = require('./router/api1.js');
+
+const app = express();
+
+app.use('/api/v1', apiVersion1);
+
+const port = process.env.PORT || 3000;
+app.listen(3000, () => {
+  console.log(`app started on port ${port}`);
+});
