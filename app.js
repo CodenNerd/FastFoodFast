ef3ad9fbfcfa5635bce4f2088998e419551452d4
AbdulAzeez <aatanda.dammy@gmail.com>
2018-09-04 15:05:05 +0100

    Create route for homepage

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const app = express();
 
 app.use('/api/v1', apiVersion1);
 
+app.get('/', (req, res) => {
+  res.send('Welcome to FastFoodFast');
+});
+
 const port = process.env.PORT || 3000;
 app.listen(3000, () => {
   console.log(`app started on port ${port}`);
