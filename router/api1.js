7671d4058f37891e463f44c2d9a26bafcdf1e17a
AbdulAzeez <aatanda.dammy@gmail.com>
2018-09-05 17:09:45 +0100

    Write route that fetches a specific order based on its id

--- router/api1.js
+++ router/api1.js
@@ -7,4 +7,11 @@ api.get('/orders', (req, res) =>{
     res.status(200).send(orders);
 })
 
+api.get('/orders/:id', (req, res) =>{
+    const order = orders.find(o => o.orderId === parseInt(req.params.id));
+    if(!order)  res.status(404).send('That paricular order was not found on our server');
+    
+    res.status(404).send(order);
+});
+
 module.exports = api;
