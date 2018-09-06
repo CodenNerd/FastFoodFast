e9890a21fe768a3c239bbe47de6dc0dc9f210aea
AbdulAzeez <aatanda.dammy@gmail.com>
2018-09-06 10:56:32 +0100

    Validate user input using Joi

--- data/newOrder.js
+++ data/newOrder.js
@@ -2,6 +2,14 @@ const Joi = require('joi');
 const orders = require('./orders');
 
 const newOrder = (req, res) => {
+  const schema = {
+    userId: Joi.number().integer().required(),
+    food: Joi.string().required(),
+  };
+
+  const result = Joi.validate(req.body, schema);
+  if (result.error) return res.status(400).send(result.error.details[0].message);
+
   const d = new Date();
   const order = {
     orderId: orders.length + 1,
@@ -13,7 +21,7 @@ const newOrder = (req, res) => {
   };
 
   orders.push(order);
-  res.status(200).send(orders);
+  return res.status(200).send(orders);
 };
 
 module.exports = newOrder;
