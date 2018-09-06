a263782c47e0dc5b9ded064a178aa6130f66a739
AbdulAzeez <aatanda.dammy@gmail.com>
2018-09-06 09:18:59 +0100

    Create router for making a new order

--- data/orders.js
+++ data/orders.js
@@ -1,36 +1,36 @@
-let orders = [
-    {
-        orderId: 1,
-        userId: 6,
-        food: ['bread'],
-        status:'accepted',
-        date: '9/5/2017',
-        time: '08:20',
-    },
-    {
-        orderId: 2,
-        userId: 8,
-        food: ['pizza','milk'],
-        status:'pending',
-        date: '4/2/2018',
-        time: '03:11',
-    },
-    {
-        orderId: 3,
-        userId: 20,
-        food: ['burrito'],
-        status:'rejected',
-        date: '9/3/2018',
-        time: '13:01',
-    },
-    {
-        orderId: 4,
-        userId: 6,
-        food: ['coke', 'burger', 'fish'],
-        status:'delivered',
-        date: '29/5/2018',
-        time: '08:15',
-    }
-]
+const orders = [
+  {
+    orderId: 1,
+    userId: 6,
+    food: ['bread'],
+    status: 'accepted',
+    date: '9/5/2017',
+    time: '08:20',
+  },
+  {
+    orderId: 2,
+    userId: 8,
+    food: ['pizza', 'milk'],
+    status: 'pending',
+    date: '4/2/2018',
+    time: '03:11',
+  },
+  {
+    orderId: 3,
+    userId: 20,
+    food: ['burrito'],
+    status: 'rejected',
+    date: '9/3/2018',
+    time: '13:01',
+  },
+  {
+    orderId: 4,
+    userId: 6,
+    food: ['coke', 'burger', 'fish'],
+    status: 'delivered',
+    date: '29/5/2018',
+    time: '08:15',
+  },
+];
 
 module.exports = orders;
--- router/api1.js
+++ router/api1.js
@@ -2,16 +2,30 @@ const express = require('express');
 const orders = require('../data/orders');
 
 const api = express.Router();
+api.use(express.json());
 
-api.get('/orders', (req, res) =>{
-    res.status(200).send(orders);
-})
-
-api.get('/orders/:id', (req, res) =>{
-    const order = orders.find(o => o.orderId === parseInt(req.params.id));
-    if(!order)  res.status(404).send('That paricular order was not found on our server');
-    
-    res.status(404).send(order);
+api.get('/orders', (req, res) => {
+  res.status(200).send(orders);
 });
 
+api.get('/orders/:id', (req, res) => {
+  const order = orders.find(o => o.orderId === Number(req.params.id));
+  if (!order) res.status(404).send('That particular order was not found on our server');
+
+  res.status(404).send(order);
+});
+
+api.post('/orders', (req, res) => {
+  const d = new Date();
+  const order = {
+    orderId: orders.length + 1,
+    userId: req.body.userId,
+    food: req.body.food,
+    status: 'pending',
+    date: `${d.getDay()}/${d.getMonth()}/${d.getFullYear()}`,
+    time: `${d.getHours()}:${d.getMinutes()}`,
+  };
+  orders.push(order);
+  res.status(200).send(orders);
+});
 module.exports = api;
