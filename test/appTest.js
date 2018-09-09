6eebfeaa4d551b3522d8b8e5658116cb099f0783
AbdulAzeez <aatanda.dammy@gmail.com>
2018-09-09 07:38:59 +0100

    Test the welcome page

--- test/appTest.js
+++ test/appTest.js
@@ -1,8 +1,15 @@
-const { assert } = require('chai');
-const app = require('../app').default;
+const request = require('supertest');
+const { expect } = require('chai');
 
-describe('App', () => {
-  it('should return hello', () => {
-    assert.equal(app(), 'hello');
+const app = require('../app');
+const orders = require('../data/orders');
+
+describe('FastFoodFast', () => {
+  describe('homepage', () => {
+    it('welcomes the user', (done) => {
+      request(app).get('/')
+        .expect(200)
+        .expect(/Welcome to FastFoodFast/, done);
+    });
   });
 });
