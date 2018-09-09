35c447100afc1201614a6a5c64186b810b0fa373
AbdulAzeez <aatanda.dammy@gmail.com>
2018-09-09 07:40:32 +0100

    Test for getting all orders

--- test/appTest.js
+++ test/appTest.js
@@ -12,4 +12,17 @@ describe('FastFoodFast', () => {
         .expect(/Welcome to FastFoodFast/, done);
     });
   });
+  describe('GET /orders ', () => {
+    it('should get all orders', (done) => {
+      request(app).get('/api/v1/orders')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end((err, res) => {
+          expect(orders.length).to.equal(res.body.length).to.equal(4);
+          expect(res.body).to.be.a('array');
+          if (err) return done(err);
+          return done();
+        });
+    });
+  });
 });
