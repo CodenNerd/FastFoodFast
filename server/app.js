5517b0435d36610fd8353d420379415db9d452eb
AbdulAzeez <aatanda.dammy@gmail.com>
2018-09-11 16:25:50 +0100

    Modify Procfile , package.json

--- server/app.js
+++ server/app.js
@@ -13,6 +13,6 @@ app.get('/', (req, res) => {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port);
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
 export default app;
