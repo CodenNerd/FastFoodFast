18c4e2c8076dcacc75d3dca7751363075b204445
AbdulAzeez <aatanda.dammy@gmail.com>
2018-09-27 07:50:50 +0100

    Include the new validation function in updateOrder

--- server/data/updateOrder.js
+++ server/data/updateOrder.js
@@ -1,5 +1,5 @@
-import joi from 'joi';
 import orders from './orders';
+import Validator from '../Validator/Validator';
 
 const updateOrder = (req, res) => {
   // Check that the request exists
@@ -7,12 +7,14 @@ const updateOrder = (req, res) => {
   if (!thisOrder) return res.status(404).send('That particular order was not found on our server');
 
   // Validation of input
-  const schema = {
-    food: joi.array(),
-    foodstatus: joi.string(),
-  };
-  const { error } = joi.validate(req.body, schema);
-  if (error) return res.status(400).send(error.details[0].message);
+
+  if (Validator(req.body.food, ['object'], res) === true) {
+    Validator(req.body.food.foodname, ['string', 'required'], res);
+    Validator(req.body.food.quantity, ['integer', 'required'], res);
+    Validator(req.body.food.price, ['number', 'required'], res);
+  }
+  Validator(req.body.foodstatus, ['string'], res);
+
 
   // Success response
   thisOrder.food = req.body.food;
