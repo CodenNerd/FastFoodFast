6d264a087e3742d23fb57c2b67ff32ec3b1b9d6c
AbdulAzeez <aatanda.dammy@gmail.com>
2018-09-29 05:34:48 +0100

    Added UI back

--- server/data/responses.js
+++
@@ -1,8 +0,0 @@
-const responses = {
-  welcome: { message: 'Welcome to FastFoodFast' },
-  pagenotfound: { message: 'Oops! That page was not found on our server. Use a different route. Error is 404' },
-  ordernotfound: { message: 'That particular order was not found on our server' },
-};
-
-
-export default responses;
