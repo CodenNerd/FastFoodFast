bb4b474f3673b0f51afe9fcad5de4976f6d9c5e1
AbdulAzeez Atanda <aatanda.dammy@gmail.com>
2018-10-01 14:18:19 +0100

    set up controller for creating new order

--- server/DB/controllers/orders.js
+++ server/DB/controllers/orders.js
@@ -1,51 +1,13 @@
-import moment from 'moment';
-import uuidv4 from 'uuid/v4';
 import db from '../db';
 
 const Order = {
 
-  async create(req, res) {
-    const text = `INSERT INTO
-      orders(id, foodname, quantity, price, foodstatus, owner_id, date_created, date_modified)
-      VALUES($1, $2, $3, $4, $5, $6, $7, $8)
-      returning *`;
-    const values = [
-      uuidv4(),
-      req.body.foodname,
-      req.body.quantity,
-      req.body.price,
-      req.body.foodstatus,
-      req.body.owner_id,
-      moment(new Date()),
-      moment(new Date()),
-    ];
-
-    try {
-      const { rows } = await db.query(text, values);
-      return res.status(201).send(rows[0]);
-    } catch (error) {
-      return res.status(400).send(error);
-    }
-  },
 
   async getAll(req, res) {
     const findAllQuery = 'SELECT * FROM orders';
     try {
       const { rows, rowCount } = await db.query(findAllQuery);
       return res.status(200).send({ rows, rowCount });
-    } catch (error) {
-      return res.status(400).send(error);
-    }
-  },
-
-  async getOne(req, res) {
-    const text = 'SELECT * FROM orders WHERE id = $1';
-    try {
-      const { rows } = await db.query(text, [req.params.id]);
-      if (!rows[0]) {
-        return res.status(404).send({ message: 'reflection not found' });
-      }
-      return res.status(200).send(rows[0]);
     } catch (error) {
       return res.status(400).send(error);
     }
@@ -65,7 +27,7 @@ const Order = {
         req.body.success || rows[0].success,
         req.body.low_point || rows[0].low_point,
         req.body.take_away || rows[0].take_away,
-        moment(new Date()),
+        new Date(),
         req.params.id,
       ];
       const response = await db.query(updateOneQuery, values);
--- server/router/api1.js
+++ server/router/api1.js
@@ -2,25 +2,23 @@ import { Router, json } from 'express';
 import dotenv from 'dotenv';
 import jsOrders from '../data/getOrders';
 import dbOrders from '../DB/controllers/orders';
-import newOrder from '../data/newOrder';
+import jsNewOrder from '../data/newOrder';
+import dbNewOrder from '../DB/controllers/newOrder';
 import updateOrder from '../data/updateOrder';
 import jsOrder from '../data/order';
 import dbOrder from '../DB/controllers/order';
 
-
 dotenv.config();
 const order = process.env.TYPE === 'db' ? dbOrder : jsOrder;
 const orders = process.env.TYPE === 'db' ? dbOrders : jsOrders;
+const newOrder = process.env.TYPE === 'db' ? dbNewOrder : jsNewOrder;
 
 const api = Router();
 api.use(json());
 
 api.get('/orders', orders);
-
 api.get('/orders/:id', order);
-
 api.post('/orders', newOrder);
-
 api.put('/orders/:id', updateOrder);
 
 export default api;
